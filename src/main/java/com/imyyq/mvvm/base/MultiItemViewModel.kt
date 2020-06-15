5c3951c0c361237173070294146d83ecfaf0ca58
imyyq-mac <imyyq.star@gmail.com>
2020-06-15 22:09:47 +0800

    update MultiItemViewModel

--- src/main/java/com/imyyq/mvvm/base/MultiItemViewModel.kt
+++ src/main/java/com/imyyq/mvvm/base/MultiItemViewModel.kt
@@ -2,7 +2,7 @@ package com.imyyq.mvvm.base
 
 open class MultiItemViewModel<VM : BaseViewModel<*>>(
     viewModel: VM,
-    position: Int? = null,
-    var mItemType: Any? = null
+    var mItemType: Any? = null,
+    position: Int? = null
 ) :
     ItemViewModel<VM>(viewModel, position)
