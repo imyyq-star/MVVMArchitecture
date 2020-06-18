02dd2a0819c118c1026b8b83f2b86a015ba2b78d
imyyq <imyyq.star@gmail.com>
2020-06-18 15:32:10 +0800

    update ReadMe

---
+++ src/main/java/com/imyyq/mvvm/binding/viewadapter/viewpager/VpFragmentPagerAdapter.kt
@@ -0,0 +1,19 @@
+package com.imyyq.mvvm.binding.viewadapter.viewpager
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.imyyq.mvvm.base.BaseFragment
+
+class VpFragmentPagerAdapter internal constructor(
+    fm: FragmentManager,
+    private val mPageList: List<BaseFragment<*, *>>
+) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    override fun getItem(i: Int): Fragment {
+        return mPageList[i]
+    }
+
+    override fun getCount(): Int {
+        return mPageList.size
+    }
+}
