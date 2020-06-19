7c2af6f7b99a5bf1d8fe72090e8f56637d95533c
imyyq <imyyq.star@gmail.com>
2020-06-19 16:16:36 +0800

    1、增加networkResponseAdapter。 2、增加状态码和状态信息文件。 3、update ReadMe。

---
+++ src/main/java/com/imyyq/mvvm/http/HttpMessage.kt
@@ -0,0 +1,5 @@
+package com.imyyq.mvvm.http
+
+const val msgEntityNullable = "实体为 null"
+const val msgEntityCodeNullable = "实体状态码为 null"
+const val msgNotHttpException = "服务端发生错误，但错误不是 HttpException！"
--- src/main/java/com/imyyq/mvvm/http/interceptor/logging/Logger.java
+++ src/main/java/com/imyyq/mvvm/http/interceptor/logging/Logger.java
@@ -1,9 +1,5 @@
 package com.imyyq.mvvm.http.interceptor.logging;
 
-import okhttp3.internal.platform.Platform;
-
 public interface Logger {
     void log(int level, String tag, String msg);
-
-    Logger DEFAULT = (level, tag, message) -> Platform.get().log(level, message, null);
 }
